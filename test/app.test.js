c38e49334da5972dc29eaece789a680848119273
Tyler O'Donnell <tyler@tylerodonnell.com>
2021-08-30 21:34:15 -0400

    test(app): add basic test to check if HTML renders

---
+++ test/app.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import App from '../src/app'
+import { StateProvider } from '../src/state'
+
+describe('<App />', () => {
+  it('renders', () => {
+    const app = render(
+      <StateProvider>
+        <App />
+      </StateProvider>
+    )
+
+    expect(app.baseElement).toMatchSnapshot()
+  })
+})
